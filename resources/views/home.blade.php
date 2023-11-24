e4f611fbfb67496b7b5f2d993329e3f656b84eda
Daniel <danyochoa112@gmail.com>
2023-11-24 09:47:17 -0600

    primeros estilos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="../resources/css/reset.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../resources/css/home.css">
+    <link rel="stylesheet" href="../resources/css/app.css">
+    <title>Welcome</title>
 </head>
-<body>
-    
-</body>
+    <header class="header_container">
+        <h3 class="title">LightDriving</h3>
+    </header>
+    <body>
+        <h4 class="eslogan"> Viaja rapido y seguro</h4>
+        <img class="banner" src="../resources/img/provisionalhome.webp" alt="banner principal">
+    </body>
+    <footer>
+
+    </footer>
 </html>
