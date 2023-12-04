09cce40b80dc0ca2e274de0afa6159817bdfbef7
Darwin224 <diromero@unah.hn>
2023-12-03 21:13:27 -0600

    perfil vista

--- resources/views/perfilConductor.blade.php
+++ resources/views/perfilConductor.blade.php
@@ -15,7 +15,7 @@
 </head>
     <header>
         <div class="header_container">
-            <a class="backbutton" href="">Volver</a>
+            <a class="backbutton" href="{{ route('conductor.informacion',$conductor->idConductor)}}" class="form_button_back">Volver</a>
             <h3 class="title">Perfil</h3>
         </div>
     </header>
@@ -25,31 +25,35 @@
         </div>
 
         <div class="info_container">
-            <p>Carlos Lainez</p>
+            <p>{{$conductor->nombre}} {{$conductor->apellido}}</p>
         </div>
 
         <p>Vehiculo Actual</p>
         <div class="car_container">
             <section class="car_card">
+                
                 <img class="icon_car" src={{asset('img/carro.png')}} alt="icono auto">
                 <div class="car_info_container">
                     <div class="car_info">
-                        <p><span class="bold">Marca: </span><span>Toyota</span></p>
-                        <p><span class="bold">Color: </span><span>Azul</span></p>
-                        <p><span class="bold">Placa: </span><span>HDO-1023</span></p>
-                        <p><span class="bold">Año: </span><span>2021</span></p>
-                        <p><span class="bold">Desde: </span><span>2023-09-12</span></p>
+                        <p><span class="bold">Marca: </span><span>{{$historico->uberActual->marca}}</span></p>
+                        <p><span class="bold">Color: </span><span>{{$historico->uberActual->color}}</span></p>
+                        <p><span class="bold">Placa: </span><span>{{$historico->uberActual->placa}}</span></p>
+                        <p><span class="bold">Año: </span><span>{{$historico->uberActual->anio}}</span></p>
+                        <p><span class="bold">Desde: </span><span>{{$historico->uberActual->fechaInicio}}</span></p>
                     </div>
-                </div>    
+                </div> 
+                <a class="dropdown-item" href="{{ route('uber.cambiar', ['idUber' => $conductor->idUber,'idConductor' => $conductor->idConductor] ) }}">Cambiar Auto</a>   
             </section>
         </div>
         
         <p>Historico vehiculos</p>
+
+        @foreach($historico->historicoUbers as $histoUber)
         <div class="accordion accordion-flush" id="accordionFlushExample">
             <div class="accordion-item">
               <h2 class="accordion-header">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                  Carro 1
+                  {{$histoUber->idHistorico}}
                 </button>
               </h2>
               <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
@@ -57,17 +61,21 @@
                     <img class="icon_car" src={{asset('img/carro.png')}} alt="icono auto">
                     <div class="car_info_container">
                         <div class="car_info">
-                            <p><span class="bold">Marca: </span><span>Toyota</span></p>
-                            <p><span class="bold">Color: </span><span>Azul</span></p>
-                            <p><span class="bold">Placa: </span><span>HDO-1023</span></p>
-                            <p><span class="bold">Año: </span><span>2021</span></p>
-                            <p><span class="bold">Desde: </span><span>2023-09-12</span></p>
+                            <p><span class="bold">Marca: </span><span>{{$histoUber->marca}}</span></p>
+                            <p><span class="bold">Color: </span><span>{{$histoUber->color}}</span></p>
+                            <p><span class="bold">Placa: </span><span>{{$histoUber->placa}}</span></p>
+                            <p><span class="bold">Año: </span><span>{{$histoUber->anio}}</span></p>
+                            <p><span class="bold">Desde: </span><span>{{$histoUber->fechaInicio}}</span></p>
                         </div>
                     </div>    
                 </section>              
               </div>
             </div>
         </div>
+        @endforeach
+
+
+
 
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
