724dccfc40ff0a1d17a6ea0e15476f464107f5eb
Daniel <danyochoa112@gmail.com>
2023-12-03 19:01:00 -0600

    se agrego vista cperfilconductor(sin datos)

--- resources/views/perfilConductor.blade.php
+++ resources/views/perfilConductor.blade.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href={{asset('../resources/css/reset.css')}}>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href={{asset('css/reset.css')}}>
+    <link rel="stylesheet" href={{asset('css/app.css')}}>
+    <link rel="stylesheet" href={{asset('css/profileConductor.css')}}>
     <title>Informacion Cliente</title>
 </head>
     <header>
@@ -18,7 +21,54 @@
     </header>
     <body>
         <div class="photo_container">
-            <img class="profile_photo" src="{{asset('img/usuarioProfileXL.png')}}" alt="">
+            <img class="profile_photo" src="{{asset('img/uberProfileXL.png')}}" alt="">
         </div>
+
+        <div class="info_container">
+            <p>Carlos Lainez</p>
+        </div>
+
+        <p>Vehiculo Actual</p>
+        <div class="car_container">
+            <section class="car_card">
+                <img class="icon_car" src={{asset('img/carro.png')}} alt="icono auto">
+                <div class="car_info_container">
+                    <div class="car_info">
+                        <p><span class="bold">Marca: </span><span>Toyota</span></p>
+                        <p><span class="bold">Color: </span><span>Azul</span></p>
+                        <p><span class="bold">Placa: </span><span>HDO-1023</span></p>
+                        <p><span class="bold">Año: </span><span>2021</span></p>
+                        <p><span class="bold">Desde: </span><span>2023-09-12</span></p>
+                    </div>
+                </div>    
+            </section>
+        </div>
+        
+        <p>Historico vehiculos</p>
+        <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                  Carro 1
+                </button>
+              </h2>
+              <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <section class="car_card">
+                    <img class="icon_car" src={{asset('img/carro.png')}} alt="icono auto">
+                    <div class="car_info_container">
+                        <div class="car_info">
+                            <p><span class="bold">Marca: </span><span>Toyota</span></p>
+                            <p><span class="bold">Color: </span><span>Azul</span></p>
+                            <p><span class="bold">Placa: </span><span>HDO-1023</span></p>
+                            <p><span class="bold">Año: </span><span>2021</span></p>
+                            <p><span class="bold">Desde: </span><span>2023-09-12</span></p>
+                        </div>
+                    </div>    
+                </section>              
+              </div>
+            </div>
+        </div>
+
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
